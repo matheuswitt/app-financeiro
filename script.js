8f320b0d6530b0cb312c575aa7809042da0f3938
matheuswitt <matheuswittkowski@gmail.com>
2025-09-01 22:04:21 -0300

    Ajustes gerais
    Troca de fonte, correções visuais, adicionado SVGs e adicionado overlay

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const sectionCadastro = document.querySelector('#cadastro-transacao-container');
 const buttonFecharCadastro = document.querySelector('#fechar-cadastro-button');
+const overlay = document.createElement('div');
 
 sectionCadastro.addEventListener('click', () => {
     sectionCadastro.style.transition = 'top 0.5s ease';
     sectionCadastro.classList.add('cadastro-aberto');
     buttonFecharCadastro.style.top = '11%';
     buttonFecharCadastro.style.transition = 'top 0.6s ease';
+    overlay.classList.add('overlay');
+    overlay.style.display = 'block';
+    setTimeout(() => {
+        overlay.classList.add('visivel');
+    }, 100);
+    document.body.appendChild(overlay);
 });
 
 buttonFecharCadastro.addEventListener('click', () => {
@@ -13,4 +20,8 @@ buttonFecharCadastro.addEventListener('click', () => {
     sectionCadastro.classList.remove('cadastro-aberto');
     buttonFecharCadastro.style.top = '95%';
     buttonFecharCadastro.style.transition = 'top 0.3s ease';
+    overlay.classList.remove('visivel');
+    setTimeout(() => {
+        overlay.classList.remove('overlay');
+    }, 300);
 });
